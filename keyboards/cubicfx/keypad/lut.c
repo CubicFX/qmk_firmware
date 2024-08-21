bc22090d974116460458e65137fb6b26c36e102f
CubicFX <167459298+CubicFX@users.noreply.github.com>
2024-08-21 19:11:46 -0400

    Keymap and info matrix

--- keyboards/cubicfx/keypad/lut.c
+++ keyboards/cubicfx/keypad/lut.c
@@ -4,16 +4,16 @@ SPDX-License-Identifier: GPL-2.0-or-later */
 #include "scanfunctions.h"
 #include "util.h"
 
-// const double a = 16654600.6755; // Lekker
-// const double b = -0.00955994866577;
-// const double c = -1278.75103145;
-// const double d = 16652478.4163;
+ const double lut_a = 16654600.6755; // Lekker
+ const double lut_b = -0.00955994866577;
+ const double lut_c = -1278.75103145;
+ const double lut_d = 16652478.4163;
 
 /* Equation parameters for the sensor-magnet linearity mapping */
-const double lut_a = 0.200347177016; // Latenpow
-const double lut_b = 0.00955994866154;
-const double lut_c = 6.01110636956;
-const double lut_d = 1966.74076381;
+// const double lut_a = 0.200347177016; // Latenpow
+// const double lut_b = 0.00955994866154;
+// const double lut_c = 6.01110636956;
+// const double lut_d = 1966.74076381;
 
 uint16_t distance_to_adc(uint8_t distance) {
     double intermediate = lut_a * exp(lut_b * distance + lut_c) + lut_d;
